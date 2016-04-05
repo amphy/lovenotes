df456d736948855f4fb425a8a3e27f7d41ad8c2a
Heather <root@turing.iamdevnull.info>
2016-04-04 21:17:45 -0400

    finished javascript portion to handle form

--- static/main.js
+++ static/main.js
@@ -1,15 +1,67 @@
+window.addEventListener("load", setup, false);
+
 var keystrokes = [];
 var timings = [];
-var textarea = document.getElementById("letter");
+var time = [];
+var textarea = null;
 
-textarea.addEventListener("keydown", record, false);
-textarea.addEventListener("keyup", stoprecord, false);
+var keys = null;
+var times = null;
+var submit = null;
 
-function record() {
+var startTime = null;
+var endTime = null;
+var delta = null;
 
+function setup() {
+
+  keys = document.getElementById("keys");
+  times = document.getElementById("times");
+
+  submit = document.getElementById("submit");
+  submit.addEventListener("click", preprocess);
+
+  textarea = document.getElementById("letter");
+  textarea.addEventListener("keydown", backspace);
+  textarea.addEventListener("keypress", record);
+  textarea.addEventListener("keyup", stoprecord);
+}
+
+function backspace(event) {
+  if(event.keyCode == 8) {
+    time.push(Date.now());
+    keystrokes.push(event.keyCode);
+    console.log("backspace");
+  }
+}
+
+function record(event) {
+  //record start time using Date
+  time.push(Date.now());
+  //store keyCode
+  keystrokes.push(event.charCode);
+  console.log(event.charCode);
 }
 
 function stoprecord() {
-
+  console.log("Time: " + time[keystrokes.length-1]);
+  //timings.push(delta);
+  //startTime = Date.now();
 }
 
+function preprocess() {
+  var text = JSON.stringify(keystrokes);
+  keys.value = text;
+
+  for(i = 0; i < time.length-1; i++){
+    delta = time[i+1]-time[i];
+    timings.push(delta);
+  }
+  timings.push(0);
+  
+  var text2 = JSON.stringify(timings);
+  times.value = text2;
+
+  console.log(text2);
+  console.log(text);
+}
